819a695c78cfcc3133ac86348b69c5c705a6f263
Evelina <kardash.evelina@chnu.edu.ua>
2024-04-04 13:26:18 +0300

    add

--- OOPLab3Tpl/OOPLab3Tpl.cpp
+++ OOPLab3Tpl/OOPLab3Tpl.cpp
@@ -1,24 +1,72 @@
-﻿// OOPLab3Tpl.cpp : Цей файл містить є шаблоном для 
-// виконання лаб. роботи №3. 
-// Він містинь функцію "main" з якої починається та закінчується виконання програми.
-//
+﻿#include <math.h>
+#endif
+#if !defined(_MSC_VER)
+#include <clocale>
+#endif
 
-#include <iostream>
+using namespace std;
 
-// Ваші файли загловки 
-//
-#include "Lab3Example.h"
-int main()
-{
-    std::cout << " Lab #3  !\n";
-//  Код виконання завдань
-//  Головне меню завдань
-//  Функції та класи можуть знаходитись в інших файлах проекту
+class triangle { // клас трикутник
+    double a; // сторона
+    unsigned int color; // колір
+public:
+    triangle() : a(1.0), color(0) {} // довжина сторони (a) на одиницю та колір на нуль
 
-    int conv = 1;
-    if (conv == 1) conv = mainExample1();
-    if (conv == 1) conv = mainExample3(); 
-    if (conv == 1) conv = mainExample4();
+    triangle(double ai) : a(ai), color(0) {} // довжина сторони (a) переданим значенням ai та колір на нуль
 
+    triangle(int ic) : a(1.0) { if (ic >= 0) color = ic; else color = 0; } // значення ic, але якщо ic менше за нуль
+
+    triangle(double a, int c) { // ініціалізує довжину сторони та колір
+        this->a = a;
+        if (c >= 0) color = c; else color = 0;
+    } // Якщо переданий колір менше за нуль, то встановлюється колір на нуль
+
+    double getA() const { return a; } // повертає довжину сторони трикутника
+
+    void setA(double a) { // встановлення нової довжини сторони трикутника
+        if (a < 0 || a > 1.e+100) {
+            cout << " Error set a \n";
+            return;
+        }
+        this->a = a;
+    } // Перевіряється чи вказане значення в межах припустимих, в іншому випадку виводиться повідомлення про помилку
+
+    double getColor() const { return color; } //  повертає колір трикутника
+
+    void setColor(int c) { // встановлення нового коліру трикутника
+        if (c < 0 || c > 10000) {
+            cout << " Error set color \n";
+            return;
+        }
+        this->color = c;
+    } // Перевіряється чи вказане значення в межах припустимих, в іншому випадку виводиться повідомлення про помилку
+
+    double S() { // площа трикутника
+        return (a * a * sqrt(3.0)) / 4;
+    }
+
+    double P() { // периметр трикутника
+        return a * 3;
+    }
+
+    void printInfo() {
+        cout << "\n a= " << a << " color = " << color;
+        cout << " S= " << S() << " P= " << P() << endl;
+    }
+};
+
+int main() {
+
+    triangle obj; // Створення об'єкту трикутника зі значеннями за замовчуванням
+    obj.printInfo();
+    double in_a; //Зчитування значень довжини сторони та коліру для створення об'єкту
+    int in_color;
+    cout << " Input side and color Icosahedron ";
+    cin >> in_a >> in_color;
+    triangle obj1(in_a), obj2(in_color), obj3(in_a, in_color); // виведення інформації про три об'єкти трикутника
+    obj1.printInfo();
+    obj2.printInfo();
+    obj3.printInfo();
+    cout << " End testing \n";
+    return 0;
 }
-
